fff7a3739851fd54804fd134e22131fee3233ce6
joshdeveloper <joshdeveloper22@gmail.com>
2024-04-27 13:53:35 +0500

    Add tooltip

--- src/app/shared/components/course-item/course-item.component.ts
+++ src/app/shared/components/course-item/course-item.component.ts
@@ -1,19 +1,17 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { ICourse } from '@models/shared/i-course';
-
+import { Component, Input, OnInit } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { ICourse } from "@models/shared/i-course";
+import { MatTooltipModule } from "@angular/material/tooltip";
 @Component({
-    selector: 'app-course-item',
-    templateUrl: 'course-item.component.html',
-    standalone: true,
-    imports: [MatButtonModule, MatIconModule]
-
+  selector: "app-course-item",
+  templateUrl: "course-item.component.html",
+  standalone: true,
+  imports: [MatButtonModule, MatIconModule, MatTooltipModule],
 })
-
 export class CourseItemComponent implements OnInit {
-    @Input() item!: ICourse;
-    @Input() purchased: boolean = false;
+  @Input() item!: ICourse;
+  @Input() purchased: boolean = false;
 
-    ngOnInit() { }
+  ngOnInit() {}
 }
