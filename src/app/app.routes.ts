f6945513f90a47fa192ad353dc3a0283969904c2
joshdeveloper <joshdeveloper22@gmail.com>
2024-04-28 13:15:58 +0500

    add 404 page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,13 @@
-import { Routes } from '@angular/router';
-import { AdminComponent } from './admin/admin.component';
-import { StudentsComponent } from './students/students.component';
-import { MentorComponent } from './mentor/mentor.component';
+import { Routes } from "@angular/router";
+import { AdminComponent } from "./admin/admin.component";
+import { StudentsComponent } from "./students/students.component";
+import { MentorComponent } from "./mentor/mentor.component";
+import { NotFoundComponent } from "./shared/components/not-found/not-found.component";
 
 export const routes: Routes = [
-    { path: 'admin', component: AdminComponent },
-    { path: 'students', component: StudentsComponent },
-    { path: 'mentor', component: MentorComponent },
+  { path: "admin", component: AdminComponent },
+  { path: "students", component: StudentsComponent },
+  { path: "mentor", component: MentorComponent },
+  { path: "not-found", component: NotFoundComponent },
+  { path: "**", redirectTo: "not-found" },
 ];
---
+++ src/app/shared/components/not-found/not-found.component.ts
@@ -0,0 +1,22 @@
+import { Component } from "@angular/core";
+import { RouterLink } from "@angular/router";
+
+@Component({
+  selector: "not-found",
+  template: `<h1>404 Error</h1>
+    <p class="zoom-area"><b>Kechirasiz.</b> Siz adashgan ko'rinasiz :(</p>
+    <section class="error-container">
+      <span class="four"><span class="screen-reader-text">4</span></span>
+      <span class="zero"><span class="screen-reader-text">0</span></span>
+      <span class="four"><span class="screen-reader-text">4</span></span>
+    </section>
+    <div class="link-container">
+      <a target="_blank" [routerLink]="['/students']" class="more-link"
+        >Ortga qaytish</a
+      >
+    </div>`,
+  standalone: true,
+  imports: [RouterLink],
+  styleUrl: "not-found.component.scss",
+})
+export class NotFoundComponent {}
